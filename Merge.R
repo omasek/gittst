842018f4d755d054500245cfafa8ca725d6c6a85
omasek <Oldrich.Masek@os-is.com>
2018-04-12 09:47:55 +0200

    ltv calculations

--- Merge.R
+++ Merge.R
@@ -12,8 +12,16 @@
 ####################################################################################
 # History:
 # 11.04.2018  Oldrich Masek     Initial version
+# 12.04.2018  Oldrich Masek     Added LTV Calculations
 ####################################################################################
 
 #clean environment 
 rm(list=ls())
 
+library(data.table)
+
+# Initial load of data
+load('c:/tmp/myFile.RData')
+
+DT[, OLTV := originalBalance / originalValuation]
+DT[, CLTV := currentBalance / currentValuation]
