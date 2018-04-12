ff02b1211eac3f423fe7fe1e093a0dbca09757e1
omasek <Oldrich.Masek@os-is.com>
2018-04-12 09:55:47 +0200

    initial version

---
+++ conflict.R
@@ -0,0 +1,27 @@
+####################################################################################
+# Description:
+#
+# conflicts sample file
+#
+####################################################################################
+# Input:
+# none
+####################################################################################
+# Output:
+# none
+####################################################################################
+# History:
+# 11.04.2018  Oldrich Masek     Initial version
+# 12.04.2018  Oldrich Masek     Added LTV Calculations
+####################################################################################
+
+#clean environment 
+rm(list=ls())
+
+library(data.table)
+
+# Initial load of data
+load('c:/tmp/myFile.RData')
+
+DT[, OLTV := currentBalance / currentValuation]
+DT[, CLTV := currentBalance / currentValuation]
