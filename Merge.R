840709ab73aebf3732722806195a127476dcdd68
omasek <Oldrich.Masek@os-is.com>
2018-04-12 11:29:33 +0200

    ss

--- Merge.R
+++ Merge.R
@@ -23,5 +23,8 @@ library(data.table)
 # Initial load of data
 load('c:/tmp/myFile.RData')
 
+# Calculte Original Loan to Value
 DT[, OLTV := originalBalance / originalValuation]
+
+# Calculte Curent Loan to Value
 DT[, CLTV := currentBalance / currentValuation]
